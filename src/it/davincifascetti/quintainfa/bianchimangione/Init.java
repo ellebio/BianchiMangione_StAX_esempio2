da7c6ca4e281eaccd2734e49a670fce5510177e6
Leonardo Bianchi <leonardo.bianchi@davincifascetti.it>
2021-11-21 19:55:04 +0100

    Final version, various corrections and adjustments

--- src/it/davincifascetti/quintainfa/bianchimangione/Init.java
+++ src/it/davincifascetti/quintainfa/bianchimangione/Init.java
@@ -1,8 +1,95 @@
+/* Authors: Leonardo Bianchi and Carlo Mangione */
+/* Version: 1.0, last modified on 21-11-21 */
+
 package it.davincifascetti.quintainfa.bianchimangione;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.namespace.QName;
+import javax.xml.stream.XMLEventReader;
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.events.*;
 
 public class Init {
 
 	public static void main(String[] args) {
+
+		List<Pacco> pacchi = new ArrayList<Pacco>();
+		Pacco paccoAttuale = null;
+
+		XMLInputFactory xmlInputFactory = XMLInputFactory.newInstance();
+		try {
+
+			XMLEventReader reader = xmlInputFactory.createXMLEventReader(new FileInputStream("files/pacco.xml"));
+			while (reader.hasNext()) {
+
+				XMLEvent nextEvent = reader.nextEvent();
+				if (nextEvent.isStartElement()) {
+
+					StartElement element = nextEvent.asStartElement();
+
+					switch (element.getName().getLocalPart()) {
+
+					case "pacchi":
+						break;
+
+					case "pacco":
+						Attribute descrizioneAttr = element.getAttributeByName(new QName("descrizione"));
+						String descrizione = descrizioneAttr.getValue();
+						Attribute dataOrdineAttr = element.getAttributeByName(new QName("dataOrdine"));
+						String dataOrdine = dataOrdineAttr.getValue();
+						Attribute dataConsegnaAttr = element.getAttributeByName(new QName("dataConsegna"));
+						String dataConsegna = dataConsegnaAttr.getValue();
+						Attribute prezzoAttr = element.getAttributeByName(new QName("prezzo"));
+						String prezzo = prezzoAttr.getValue();
+						paccoAttuale = new Pacco(descrizione, dataOrdine, dataConsegna, prezzo);
+						pacchi.add(paccoAttuale);
+
+						break;
+
+					}
+
+				}
+
+				if (nextEvent.isEndElement()) {
+
+					EndElement element = nextEvent.asEndElement();
+
+					switch (element.getName().getLocalPart()) {
+
+					case "pacchi":
+						break;
+
+					}
+
+				}
+
+			}
+
+			System.out.print("FINISHED READING!\n\n");
+
+			for (int i = 0; i < pacchi.size(); i++) {
+
+				System.out.print(pacchi.get(i).getDetails());
+
+			}
+
+		} catch (FileNotFoundException e) {
+
+			System.out.print("File not found! " + e + ".\n");
+
+		} catch (XMLStreamException e) {
+
+			System.out.print("Stream exception! " + e + ".\n");
+
+		} catch (Exception e) {
+
+			System.out.print("Exception! " + e + ".\n");
+
+		}
 
 	}
 
--- src/it/davincifascetti/quintainfa/bianchimangione/Pacco.java
+++ src/it/davincifascetti/quintainfa/bianchimangione/Pacco.java
@@ -1,8 +1,54 @@
+/* Authors: Leonardo Bianchi and Carlo Mangione */
+/* Version: 1.0, last modified on 21-11-21 */
+
 package it.davincifascetti.quintainfa.bianchimangione;
 
 public class Pacco {
 
-	public Pacco() {
+	private String descrizione, dataOrdine, dataConsegna, prezzo;
+
+	public Pacco(String descrizione, String dataOrdine, String dataConsegna, String prezzo) {
+
+		this.descrizione = descrizione;
+		this.dataOrdine = dataOrdine;
+		this.dataConsegna = dataConsegna;
+		this.prezzo = prezzo;
+
+	}
+
+	public boolean isComplete() {
+
+		if (this.descrizione.isBlank() || this.descrizione.isEmpty() || this.dataOrdine.isEmpty()
+				|| this.dataOrdine.isBlank() || this.dataConsegna.isBlank() || this.dataConsegna.isEmpty()
+				|| this.prezzo.isBlank() || this.prezzo.isEmpty()) {
+
+			return false;
+
+		} else {
+
+			return true;
+
+		}
+
+	}
+
+	public String getDetails() throws Exception {
+
+		if (this.isComplete()) {
+
+			String toReturn = "+-----------------------------------+\n";
+			toReturn += "Descrizione pacco: " + this.descrizione + "\n";
+			toReturn += "Ordinato in data " + this.dataOrdine + " e consegnato in data " + this.dataConsegna + "\n";
+			toReturn += "Costo ordine: " + this.prezzo + "€\n";
+
+			return toReturn;
+
+		} else {
+
+			throw new Exception("Errore, le informazioni del pacco non sono complete");
+
+		}
+
 	}
 
 }
